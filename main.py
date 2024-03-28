59f97cb0505eb59f644fcc2a8a3f0b313052feec
Cecil Wagner <wagnercecil@gmail.com>
2024-03-28 16:03:53 +0100

    Added dynamic run command, and plotting functions

--- main.py
+++ main.py
@@ -9,89 +9,8 @@ import pandas as pd
 
 
 import config
-
-EXTENSIONS = [
-    "+xseal5testalucvabs16",
-    "+xseal5testalucvabs32",
-    "+xseal5testalucvabs8",
-    "+xseal5testalucvaddnrsi16",
-    "+xseal5testalucvaddnrsi32",
-    "+xseal5testalucvaddnrui16",
-    "+xseal5testalucvaddnrui32",
-    "+xseal5testalucvaddns",
-    "+xseal5testalucvaddnu",
-    "+xseal5testalucvaddrnrsi16",
-    "+xseal5testalucvaddrnrsi32",
-    "+xseal5testalucvaddrnrui16",
-    "+xseal5testalucvaddrnrui32",
-    "+xseal5testalucvaddrns",
-    "+xseal5testalucvaddrnu",
-    "+xseal5testalucvextbs",
-    "+xseal5testalucvextbz",
-    "+xseal5testalucvexths",
-    "+xseal5testalucvexthz",
-    "+xseal5testalucvmaxi1216",
-    "+xseal5testalucvmaxi1232",
-    "+xseal5testalucvmaxi516",
-    "+xseal5testalucvmaxi532",
-    "+xseal5testalucvmaxs16",
-    "+xseal5testalucvmaxs32",
-    "+xseal5testalucvmaxs8",
-    "+xseal5testalucvmaxu16",
-    "+xseal5testalucvmaxu32",
-    "+xseal5testalucvmaxu8",
-    "+xseal5testalucvmini1216",
-    "+xseal5testalucvmini1232",
-    "+xseal5testalucvmini516",
-    "+xseal5testalucvmini532",
-    "+xseal5testalucvmins16",
-    "+xseal5testalucvmins32",
-    "+xseal5testalucvmins8",
-    "+xseal5testalucvminu16",
-    "+xseal5testalucvminu32",
-    "+xseal5testalucvminu8",
-    "+xseal5testalucvsletsi16",
-    "+xseal5testalucvsletsi32",
-    "+xseal5testalucvsletui16",
-    "+xseal5testalucvsletui32",
-    "+xseal5testalucvsubnrsi16",
-    "+xseal5testalucvsubnrsi32",
-    "+xseal5testalucvsubnrui16",
-    "+xseal5testalucvsubnrui32",
-    "+xseal5testalucvsubns",
-    "+xseal5testalucvsubnu",
-    "+xseal5testalucvsubrnrsi16",
-    "+xseal5testalucvsubrnrsi32",
-    "+xseal5testalucvsubrnrui16",
-    "+xseal5testalucvsubrnrui32",
-    "+xseal5testalucvsubrns",
-    "+xseal5testalucvsubrnu",
-    "+xseal5testmaccvmachhns",
-    "+xseal5testmaccvmachhnu",
-    "+xseal5testmaccvmachhrns",
-    "+xseal5testmaccvmachhrnu",
-    "+xseal5testmaccvmacns",
-    "+xseal5testmaccvmacnu",
-    "+xseal5testmaccvmacrns",
-    "+xseal5testmaccvmacrnu",
-    "+xseal5testmaccvmacsi16",
-    "+xseal5testmaccvmacsi32",
-    "+xseal5testmaccvmacui16",
-    "+xseal5testmaccvmacui32",
-    "+xseal5testmaccvmsusi16",
-    "+xseal5testmaccvmsusi32",
-    "+xseal5testmaccvmsuui16",
-    "+xseal5testmaccvmsuui32",
-    "+xseal5testmaccvmulhhns",
-    "+xseal5testmaccvmulhhnu",
-    "+xseal5testmaccvmulhhrns",
-    "+xseal5testmaccvmulhhrnu",
-    "+xseal5testmaccvmulns",
-    "+xseal5testmaccvmulnu",
-    "+xseal5testmaccvmulrns",
-    "+xseal5testmaccvmulrnu",
-    "+grp32v",
-]
+from config import EXTENSIONS
+import plot
 
 
 def compile_benchmarks(benchmarks: list[str]):
@@ -119,7 +38,7 @@ def compile_benchmarks(benchmarks: list[str]):
             "config2cols",
             "-c",
             "config2cols.limit=mlif.global_isel,auto_vectorize.custom_unroll",
-            "-v",
+            # "-v",
             "--parallel",
             "-c",
             f"llvm.install_dir={config.LLVM_PATH}",
@@ -167,6 +86,7 @@ def remove_unused_extensions(
     report = pd.read_csv(config.MLONMCU_HOME + "/temp/sessions/latest/report.csv")
     resultless_benchmarks = report.loc[report["DumpCountsGen"] == "{}"]["Model"]
 
+    # TODO add more information to the used instructions, e.g. in which benchmark were they used
     used_instructions: dict[str, int] = {}
     for series in report["DumpCountsGen"]:
         d: dict = ast.literal_eval(series)
@@ -191,11 +111,15 @@ def instruction_to_extension(instructions: list[str]):
     return extensions
 
 
-def compare_static_counts(new_run: bool, benchmarks: list[str], extensions: list[str]):
+def compare_static_counts(
+    benchmarks: list[str], extensions: list[str], new_run: bool = True
+):
     """
     Runs a list of benchmarks, with and without the extensions,
     and compares the static code size
     """
+    # TODO as im only looking at the static code size anyway,
+    # this could be combined with the compile flow as it would make no difference
     if new_run:
         subprocess.run(
             [
@@ -217,7 +141,7 @@ def compare_static_counts(new_run: bool, benchmarks: list[str], extensions: list
                 "config2cols",
                 "-c",
                 "config2cols.limit=mlif.global_isel,auto_vectorize.custom_unroll",
-                "-v",
+                # "-v",
                 "--parallel",
                 "-c",
                 f"llvm.install_dir={config.LLVM_PATH}",
@@ -254,17 +178,117 @@ def compare_static_counts(new_run: bool, benchmarks: list[str], extensions: list
             cwd=config.MLONMCU_PATH,
         )
     report = pd.read_csv(config.MLONMCU_HOME + "/temp/sessions/latest/report.csv")
+    return report
+
+
+def compare_dynamic_counts(
+    benchmarks: list[str], extensions: list[str], new_run: bool = True
+):
+    """
+    Runs a list of benchmarks, with and without the extensions,
+    It generates an instruction trace, so it takes a while,
+    The trace gets filtered for custom instructions
+    It also compares the code size
+    """
+    if new_run:
+        subprocess.run(
+            [
+                "./venv/bin/python",
+                "-m",
+                "mlonmcu.cli.main",
+                "flow",
+                "run",
+                *benchmarks,
+                "--target",
+                "etiss",
+                "-c",
+                "mlif.toolchain=llvm",
+                "-c",
+                "mlif.extend_attrs=1",
+                "--config-gen",
+                "mlif.global_isel=1",
+                "--post",
+                "config2cols",
+                "-c",
+                "config2cols.limit=mlif.global_isel,auto_vectorize.custom_unroll",
+                # "-v",
+                "--parallel",
+                "-c",
+                f"llvm.install_dir={config.LLVM_PATH}",
+                "--post",
+                "rename_cols",
+                "-c",
+                "rename_cols.mapping={'config_mlif.global_isel':'GIsel',"
+                "'config_auto_vectorize.custom_unroll':'Unroll'}",
+                "-f",
+                "log_instrs",
+                "-c",
+                "log_instrs.to_file=1",
+                "--post",
+                "analyse_instructions",
+                "-c",
+                "analyse_instructions.to_df=1",
+                "-c",
+                "analyse_instructions.seq_depth=1",
+                "-c",
+                "analyse_instructions.top=100000",
+                "--post",
+                "filter_cols",
+                "-c",
+                "filter_cols.keep=Model,ROM code,GIsel,Reason,"
+                "TraceCountsGen,Total Instructions,Unroll",
+                "-c",
+                "mlif.strip_strings=0",
+                "-c",
+                "analyse_dump.to_df=1",
+                "-c",
+                f"etissvp.script={config.ETISS_RUN_HELPER}",
+                "--post",
+                "compare_rows",
+                "-c",
+                "compare_rows.to_compare=Total Instructions",
+                "-f",
+                "auto_vectorize",
+                "--config-gen3",
+                "auto_vectorize.custom_unroll=1",
+                "--config-gen2",
+                'etiss.attr="+m"',
+                "--config-gen2",
+                f"etiss.attr=\"+m,{','.join(extensions)}\"",
+            ],
+            check=True,
+            cwd=config.MLONMCU_PATH,
+        )
+    report = pd.read_csv(config.MLONMCU_HOME + "/temp/sessions/latest/report.csv")
+    return report
 
 
 def main():
     """Main entry"""
-    benchmarks = ["coremark", "dhrystone"]
-    used_instructions, useless_benchmarks = remove_unused_extensions(benchmarks, True)
-    # remove unused benchmarks
-    new_benchmarks = [bench for bench in benchmarks if bench not in useless_benchmarks]
+    benchmarks2 = ["coremark", "dhrystone"]
+    benchmarks = ["coremark"]
+    # used_instructions, ineffective_benchmarks = remove_unused_extensions(
+    #     benchmarks, True
+    # )
+    # # remove unused benchmarks
+    # used_benchmarks = [
+    #     bench for bench in benchmarks if bench not in ineffective_benchmarks
+    # ]
+    # # TODO i could add a validate run before or after the static count
+    # used_extensions = instruction_to_extension(list(used_instructions.keys()))
+    # static_report = compare_static_counts(used_benchmarks, used_extensions)
+    # dynamic_report = compare_dynamic_counts(used_benchmarks, used_extensions)
+    dynamic_report = compare_dynamic_counts(benchmarks, EXTENSIONS, False)
+    plot.compare_relative_inst_count(dynamic_report)
+    # plot.instruction_count(data=used_instructions)
+    plot.instruction_count_across_benchmarks(df=dynamic_report)
 
-    # compare_static_counts(True, benchmarks, new_extensions)
+
+def test_plots():
+    df = pd.read_csv("/home/cecil/Documents/Cecil/cecil_bench_v1/cecil_other_trace.csv")
+    plot.instruction_count_across_benchmarks(df)
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    test_plots()
--- plot.py
+++ plot.py
@@ -1,40 +1,108 @@
+"""Functions to plot the reports"""
+
+import ast
 import matplotlib.pyplot as plt
-import subprocess
+import matplotlib.axes
+import pandas as pd
 
 
-def plot():
+# TODO adapt functions to use the report df
+
+
+def dump(row_count: int = 20):
+    """Plots the default mlonmcu instruction dump"""
+    path = "dump_counts.csv"
+    instruction_counts = pd.read_csv(path)
+    instruction_counts.fillna("UNKNOWN", inplace=True)
+    # the data is already sorted in descending order
+    instruction_counts.sort_values(by=["Count"], inplace=True)
+    top_n = instruction_counts.head(n=row_count)
+
+    fig, ax = plt.subplots()
+    # plt.subplots_adjust(bottom=0.25)
+    ax.bar(x=top_n["Instruction"], height=top_n["Count"])
+    ax.tick_params(axis="x", labelrotation=90)
+
+    plt.show()
+
+
+def analyse_inst(df: pd.DataFrame):
+    """Plots the Major Opcode against the instruction Count"""
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.25)
+    ax.bar(x=df["Major"], height=df["Count"])
+    ax.tick_params(axis="x", labelrotation=90)
+
+    plt.show()
+
+
+def instruction_count(data: dict):
+    """Plots data.key against data.value"""
     fig = plt.figure()
-    ax = fig.subplots()
-    pass
+    ax: matplotlib.axes.Axes = fig.subplots()  # type: ignore
+    x, y = zip(*sorted(data.items(), key=lambda x: x[1]))
+    ax.bar(x=x, height=y)
+    # ax.plot("Instruction", "Count", data=data)
+    plt.show()
 
 
-def main():
-    subprocess.run(
-        [
-            "python3",
-            "-m",
-            "mlonmcu.cli.main",
-            "flow",
-            "run",
-            "sine_model",
-            "--target",
-            "etiss",
-            "--config-gen",
-            "etiss.arch=rv32imfdc",
-            "etiss.cpu_arch=RV32IMACFD",
-            "--config-gen",
-            "etiss.arch=rv32imafdc",
-            "etiss.cpu_arch=RV32IMACFD",
-            "--parallel",
-            "--post",
-            "config2cols",
-            "-c",
-            "config2cols.limit=etiss.arch,etiss.cpu_arch,mlif.optimize",
-        ],
-        check=True,
-        cwd="./mlonmcu",
-    )
+def instruction_count_across_benchmarks_wip(
+    df: pd.DataFrame, dynamic_count: bool = True
+):
+    """df: DataFrame containing the instruction count across models
+    This is currently not working as the bars are not stacked"""
+    col_name = "TraceCountsGen" if dynamic_count else "DumpCountsGen"
+    # Removing Benchmarks with no generated instructions and turning the "dicts" into real dicts
+    df2 = df[df[col_name] != "{}"]
+    df2.loc[:, col_name] = df2[col_name].apply(ast.literal_eval)
+
+    fig = plt.figure()
+    ax: matplotlib.axes.Axes = fig.subplots()  # type: ignore
+
+    for i, row in df2.iterrows():
+        instr_count: dict[str, int] = row[col_name]
+        instructions, counts = zip(*sorted(instr_count.items(), key=lambda x: x[1]))
+        ax.barh(y=instructions, width=counts, label=row["Model"])
+
+    ax.legend(loc="upper right")
+
+    plt.show()
 
 
-if __name__ == "__main__":
-    main()
+def instruction_count_across_benchmarks(df: pd.DataFrame, dynamic_count: bool = True):
+    """df: DataFrame containing the instruction count across models"""
+    col_name = "TraceCountsGen" if dynamic_count else "DumpCountsGen"
+    # Removing Benchmarks with no generated instructions and turning the "dicts" into real dicts
+    df2 = df[df[col_name] != "{}"]
+    df2.loc[:, col_name] = df2[col_name].apply(ast.literal_eval)
+
+    # TODO this is a somewhat dirty hack, not sure how to create the df idiomaticly
+    d = {}
+    for _, row in df2.iterrows():
+        d[row["Model"]] = row[col_name]
+    new_df = pd.DataFrame.from_dict(d)
+
+    ax = new_df.plot.barh(stacked=True)
+
+    plt.show()
+
+
+def compare_relative_inst_count(df: pd.DataFrame):
+    """Plots the relative Instruction count for each benchmark"""
+    bench_instr_count = df.loc[df["Total Instructions (rel.)"] != 1.0]
+    ax = df.plot.bar(x="Model", y="Total Instructions (rel.)", rot=0)
+    plt.show()
+
+
+def compare_runs(report: pd.DataFrame):
+    # This should be the same as in one of the mlonmcu examples
+    columns = ["ROM code", "Total Instructions"]
+
+    # collumns to plot: ROM code & Total Instructions
+    fig, axs = plt.subplots(1, len(columns))
+    axs: list[matplotlib.axes.Axes]
+    for i, col in enumerate(columns):
+        # axs[i].bar(x=report["config_etiss.arch"].astype(str), height=report[col])
+        report.plot(x="config_etiss.arch", y=col, kind="bar", ax=axs[i])
+
+    plt.show()
